ddbe634de685000c2cbb01284c8d1dc54832994e
Victor Matheus <victormatheus507@gmail.com>
2024-01-17 15:55:11 -0300

    comparando variaveis

--- LP-introducao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ LP-introducao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,4 +1,7 @@
 alert('Boas Vindas ao jogo do número secreto')
 let numeroSecreto = 29
+let chute = prompt('Digite um número entre 1 e 30')
 
-prompt('Digite um número entre 1 e 30')
+if(numeroSecreto == chute) {
+  alert('Acertou')
+}
