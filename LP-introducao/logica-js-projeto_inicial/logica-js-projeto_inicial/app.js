7fd72847e489b657df14f52bde93665f68b9d3f3
Victor Matheus <victormatheus507@gmail.com>
2024-01-17 16:09:00 -0300

    else e comentarios

--- LP-introducao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ LP-introducao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,7 +1,11 @@
 alert('Boas Vindas ao jogo do número secreto')
 let numeroSecreto = 29
+console.log(numeroSecreto);
 let chute = prompt('Digite um número entre 1 e 30')
 
+// se o chute for igual ao número secreto
 if(numeroSecreto == chute) {
   alert('Acertou')
-}
+} else {
+  alert('Você errou')
+} 
