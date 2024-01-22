1ace01dfb68e2f35b1a9ad674cd5d640d5acc2e7
Victor Matheus <victormatheus507@gmail.com>
2024-01-22 16:09:53 -0300

    break

--- LP-introducao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ LP-introducao/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,19 +1,27 @@
 alert('Boas Vindas ao jogo do número secreto');
 let numeroSecreto = 5;
 console.log(numeroSecreto);
-let chute
+let chute;
+let tentativas = 1
 
 // enquanto
 while (chute != numeroSecreto) {
   chute = prompt('Digite um número entre 1 e 30');
   // se o chute for igual ao número secreto
   if (chute == numeroSecreto) {
-    alert(`Isso ai ! Você descobriu o número secreto (${numeroSecreto})`);
+    break
   } else {
     if (chute > numeroSecreto) {
       alert(`O número secreto é menor que ${chute}`);
     } else {
       alert(`O número secreto é maior que ${chute}`);
     }
+    tentativas++
   }
+}
+
+if (tentativas > 1) {
+  alert(`Isso ai ! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas`);
+} else {
+  alert(`Isso ai ! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa`);
 }
