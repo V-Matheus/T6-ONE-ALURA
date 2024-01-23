7d350c4ffae34f4c195b9cadd7c3613e244e4866
Victor Matheus <victormatheus507@gmail.com>
2024-01-23 16:01:15 -0300

    botao reiniciar

--- LP-introducao/logica-js-function-and-lists/app.js
+++ LP-introducao/logica-js-function-and-lists/app.js
@@ -1,5 +1,5 @@
 let numeroSecreto = gerarNumeroAleatorio();
-let tentativas = 1
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
@@ -10,20 +10,27 @@ exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 function verificarChute() {
   let chute = document.querySelector('input').value;
   if (chute == numeroSecreto) {
-    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-    let mensagemTentativas = `Você acetou o número secreto com ${tentativas} ${palavraTentativa}`
+    let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+    let mensagemTentativas = `Você acetou o número secreto com ${tentativas} ${palavraTentativa}`;
     exibirTextoNaTela('h1', 'Acertou!');
     exibirTextoNaTela('p', mensagemTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
   } else {
     if (chute > numeroSecreto) {
       exibirTextoNaTela('p', 'O número secreto é menor');
     } else {
       exibirTextoNaTela('p', 'O número secreto é maior');
     }
-    tentativas++
+    tentativas++;
+    limparCampo();
   }
 }
 
 function gerarNumeroAleatorio() {
   return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+  chute = document.querySelector('input');
+  chute.value = '';
 }
