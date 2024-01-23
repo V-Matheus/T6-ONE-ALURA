93b3cd15fd54696ea7c89799aceff8e60a9c436b
Victor Matheus <victormatheus507@gmail.com>
2024-01-23 14:57:30 -0300

    manipulando texto

---
+++ LP-introducao/logica-js-function-and-lists/app.js
@@ -0,0 +1,7 @@
+let titulo = document.querySelector('h1');
+
+titulo.innerHTML = 'Jogo do Número secreto';
+
+
+let paragrafo = document.querySelector('.texto__paragrafo')
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10'
