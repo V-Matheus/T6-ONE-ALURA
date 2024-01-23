19173809c8498da1dbe126d48fceb0e6917b6040
Victor Matheus <victormatheus507@gmail.com>
2024-01-23 15:48:09 -0300

    verifiicando o chute

--- LP-introducao/logica-js-function-and-lists/app.js
+++ LP-introducao/logica-js-function-and-lists/app.js
@@ -4,10 +4,20 @@ function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
 }
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
 
 function verificarChute() {
-  let chute = document.querySelector('input').value
-  console.log(chute == numeroSecreto);
+  let chute = document.querySelector('input').value;
+  if (chute == numeroSecreto) {
+    exibirTextoNaTela('h1', 'Acertou!');
+    exibirTextoNaTela('p', 'Você acetou o número secreto');
+  } else {
+    if (chute > numeroSecreto) {
+      exibirTextoNaTela('p', 'O número secreto é menor');
+    } else {
+      exibirTextoNaTela('p', 'O número secreto é maior');
+    }
+  }
 }
 
 function gerarNumeroAleatorio() {
