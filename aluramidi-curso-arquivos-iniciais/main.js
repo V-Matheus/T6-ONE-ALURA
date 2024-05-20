9a57e1b956aeba32b97b7d725bb3015e84ee86d3
Victor Matheus <victormatheus507@gmail.com>
2024-05-20 16:13:55 -0300

    vericando se a funcao foi chamada corretamente

--- aluramidi-curso-arquivos-iniciais/main.js
+++ aluramidi-curso-arquivos-iniciais/main.js
@@ -1,5 +1,8 @@
-function tocaSom(idElementoAudio) {
-  document.querySelector(idElementoAudio).play();
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento || elemento.localName !== 'audio') alert('Elemento não encontrado');
+  elemento.play();
 }
 
 const litaDeTeclas = document.querySelectorAll('.tecla');
@@ -8,14 +11,14 @@ for (let contador = 0; contador < litaDeTeclas.length; contador++) {
   const tecla = litaDeTeclas[contador];
   const instrumento = tecla.classList[1];
 
-  const idAudio = `#som_${instrumento}`
+  const idAudio = `#som_${instrumento}`;
 
   tecla.onclick = () => tocaSom(idAudio);
 
   tecla.onkeydown = (evento) => {
-    if(evento.code === 'Space' || evento.code === 'Enter') {
-      tecla.classList.add('ativa')
+    if (evento.code === 'Space' || evento.code === 'Enter') {
+      tecla.classList.add('ativa');
     }
-  }
-  tecla.onkeyup = () => tecla.classList.remove('ativa')
+  };
+  tecla.onkeyup = () => tecla.classList.remove('ativa');
 }
