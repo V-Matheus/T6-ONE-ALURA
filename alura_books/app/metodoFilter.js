377f94920c2fa346ed5238dfd9d2e45a5b874413
Victor Matheus <victormatheus507@gmail.com>
2024-05-22 15:56:36 -0300

    valor total com reduce

--- alura_books/app/metodoFilter.js
+++ alura_books/app/metodoFilter.js
@@ -8,10 +8,10 @@ function filtrarLivros() {
     categoria == 'disponivel'
       ? filtrarPorDisponibilidade()
       : filtrarPorCategoria(categoria);
-  console.log(livrosFiltrados);
   exibirOsLivrosNaTela(livrosFiltrados);
   if (categoria == 'disponivel') {
-    exibirValorTotalDosLivrosDisponiveisNaTela();
+    const valorTotal = calcularValorTotalDeLivrosDisponiveis(livrosFiltrados);
+    exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal);
   }
 }
 
@@ -20,13 +20,13 @@ function filtrarPorCategoria(categoria) {
 }
 
 function filtrarPorDisponibilidade() {
-  return livros.filter(livro => livro.quantidade > 0);
+  return livros.filter((livro) => livro.quantidade > 0);
 }
 
-function exibirValorTotalDosLivrosDisponiveisNaTela() {
+function exibirValorTotalDosLivrosDisponiveisNaTela(valorTotal) {
   elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
   <div class="livros__disponiveis">
-      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+      <p>Todos os livros disponíveis por R$ <span id="valor">${valorTotal}</span></p>
     </div>  
 
   `;
