38973d26f1e9797482363697457f875f04d72df4
Victor Matheus <victormatheus507@gmail.com>
2024-05-22 14:37:38 -0300

    desconto com map

--- alura_books/app/main.js
+++ alura_books/app/main.js
@@ -1,33 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let livros = [];
-  const endpointDaApi =
-    'https://guilhermeonrails.github.io/casadocodigo/livros.json';
-  const elementoParaInserirLivros = document.getElementById('livros');
+let livros = [];
+const endpointDaApi =
+  'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 
-  async function getBuscarLivroDaApi() {
-    const res = await fetch(endpointDaApi);
-    livros = await res.json();
-    exibirOsLivrosNaTela(livros);
-  }
+async function getBuscarLivroDaApi() {
+  const res = await fetch(endpointDaApi);
+  livros = await res.json();
+  let livrosComDesconto = aplicarDesconto(livros)
+  exibirOsLivrosNaTela(livrosComDesconto);
+}
 
-  function exibirOsLivrosNaTela(listaDeLivros) {
-    listaDeLivros.forEach((livro) => {
-      elementoParaInserirLivros.innerHTML += `
-      <div class="livro">
-      <img class="livro__imagens" src="${livro.imagem}"
-        alt="${livro.alt}" />
-      <h2 class="livro__titulo">
-        ${livro.titulo}
-      </h2>
-      <p class="livro__descricao">${livro.autor}</p>
-      <p class="livro__preco" id="preco">R$${livro.preco}</p>
-      <div class="tags">
-        <span class="tag">${livro.categoria}</span>
-      </div>
-    </div>
-      `;
-    });
-  }
-
-  getBuscarLivroDaApi();
-});
+getBuscarLivroDaApi();
