10d216a9c00aa2c7e22cc6d0a7e6dc4d84d36559
Victor Matheus <victormatheus507@gmail.com>
2024-05-22 15:50:15 -0300

    filtrando por disponibilidade

--- alura_books/app/metodoFilter.js
+++ alura_books/app/metodoFilter.js
@@ -1,11 +1,33 @@
-const botoes = document.querySelectorAll('.btn')
-botoes.forEach((btn) => btn.addEventListener('click', filtrarLivros))
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach((btn) => btn.addEventListener('click', filtrarLivros));
 
 function filtrarLivros() {
-  const elementoBtn = document.getElementById(this.id)
-  const categoria = elementoBtn.value
-  let livrosFiltrados = livros.filter(
-    (livro) => livro.categoria === categoria,
-  );
- exibirOsLivrosNaTela(livrosFiltrados);
+  const elementoBtn = document.getElementById(this.id);
+  const categoria = elementoBtn.value;
+  let livrosFiltrados =
+    categoria == 'disponivel'
+      ? filtrarPorDisponibilidade()
+      : filtrarPorCategoria(categoria);
+  console.log(livrosFiltrados);
+  exibirOsLivrosNaTela(livrosFiltrados);
+  if (categoria == 'disponivel') {
+    exibirValorTotalDosLivrosDisponiveisNaTela();
+  }
+}
+
+function filtrarPorCategoria(categoria) {
+  return livros.filter((livro) => livro.categoria == categoria);
+}
+
+function filtrarPorDisponibilidade() {
+  return livros.filter(livro => livro.quantidade > 0);
+}
+
+function exibirValorTotalDosLivrosDisponiveisNaTela() {
+  elementoComValorTotalDeLivrosDisponiveis.innerHTML = `
+  <div class="livros__disponiveis">
+      <p>Todos os livros disponíveis por R$ <span id="valor">299,00</span></p>
+    </div>  
+
+  `;
 }
