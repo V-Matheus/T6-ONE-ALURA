161b7d2b303348a86283d36c1fcfef4147f2ee86
Victor Matheus <victormatheus507@gmail.com>
2024-05-27 15:41:28 -0300

    tratando erros

--- js-consumindo-dados-api-main/script.js
+++ js-consumindo-dados-api-main/script.js
@@ -1,7 +1,12 @@
 async function buscaEndeco() {
-  const consultaCep = await fetch('https://viacep.com.br/ws/01001000/json');
-  const consultaCepConvertida = await consultaCep.json()
-  console.log(consultaCepConvertida);
+  try {
+    const consultaCep = await fetch('https://viacep.com.br/ws/01001000/json');
+    const consultaCepConvertida = await consultaCep.json();
+    if(consultaCepConvertida.erro) throw Error('Cep não existe')
+    console.log(consultaCepConvertida);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 buscaEndeco();
