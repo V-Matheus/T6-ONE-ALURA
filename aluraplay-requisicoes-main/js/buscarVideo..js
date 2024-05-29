731a7d644e63349a185a222b50b312023a44213a
Victor Matheus <victormatheus507@gmail.com>
2024-05-29 17:25:30 -0300

    reunitilizar funcoes

--- aluraplay-requisicoes-main/js/buscarVideo..js
+++ aluraplay-requisicoes-main/js/buscarVideo..js
@@ -1,14 +1,25 @@
 import { buscarVideo } from './conectaApi.js';
+import { constroiCard } from './mostrarVideos.js';
 
 async function buscarVideos(evento) {
   evento.preventDefault();
 
   const dadosDePesquisa = document.querySelector('[data-pesquisa]').value;
   const busca = await buscarVideo(dadosDePesquisa);
-  console.log(busca);
+
+  const lista = document.querySelector('[data-lista]');
+
+  busca.forEach((elemento) =>
+    lista.appendChild(
+      constroiCard(
+        elemento.titulo,
+        elemento.descricao,
+        elemento.url,
+        elemento.imagem,
+      ),
+    ),
+  );
 }
 
 const botaoDePesquisa = document.querySelector('[data-botao-pesquisa]');
-console.log(botaoDePesquisa);
-
 botaoDePesquisa.addEventListener('click', (evento) => buscarVideos(evento));
