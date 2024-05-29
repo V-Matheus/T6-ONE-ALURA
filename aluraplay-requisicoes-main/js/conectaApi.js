dc59d04b76e256fa7820f10ee8f28b50a2398301
Victor Matheus <victormatheus507@gmail.com>
2024-05-29 16:03:02 -0300

    forEatch

--- aluraplay-requisicoes-main/js/conectaApi.js
+++ aluraplay-requisicoes-main/js/conectaApi.js
@@ -1,11 +1,7 @@
-async function listaVideos() {
+export async function listaVideos() {
   const conexao = await fetch('http://localhost:3000/videos')
   const conexaoConvertida = await conexao.json()
   
-  
+  console.log(conexaoConvertida);
   return conexaoConvertida
-}
-
-export const conectaApi = {
-  listaVideos
 }
