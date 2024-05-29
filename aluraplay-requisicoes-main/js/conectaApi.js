f245a153d806760d44aef036547f49425ab5487f
Victor Matheus <victormatheus507@gmail.com>
2024-05-29 16:13:17 -0300

    requisicao POST

--- aluraplay-requisicoes-main/js/conectaApi.js
+++ aluraplay-requisicoes-main/js/conectaApi.js
@@ -5,3 +5,21 @@ export async function listaVideos() {
   console.log(conexaoConvertida);
   return conexaoConvertida
 }
+
+export async function criaVideo(titulo, descricao, url, imagem) {
+  const conexao = await fetch('http://localhost:3000/videos', {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({
+      titulo,
+      descricao: `${descricao} mil visualizações`,
+      url,
+      imagem,
+    })
+  })
+
+  const conexaoConvertida = await conexao.json()
+  return conexaoConvertida
+}
