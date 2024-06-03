a29e76ea24d6ec73a0e14c3447b05717e27bf157
Victor Matheus <victormatheus507@gmail.com>
2024-06-03 16:39:46 -0300

    props

--- organo/src/components/CampoTexto/index.jsx
+++ organo/src/components/CampoTexto/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import './CampoTexto.css'
 
-export const CampoTexto = () => {
+export const CampoTexto = ({label, placeholder}) => {
   return (
     <div className='campo-texto'>
-      <label htmlFor="">Nome</label>
-      <input type="text" />
+      <label htmlFor="">{label}</label>
+      <input type="text" placeholder={placeholder} />
     </div>
   )
 }
