2ca2b8c5904ec02e57c6bee4510e5f00034163ec
Victor Matheus <victormatheus507@gmail.com>
2024-06-04 15:51:28 -0300

    Card de colaborador

---
+++ organo/src/components/Colaborador/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Colaborador.css'
+
+export const Colaborador = () => {
+  return (<div className='colaborador'> 
+      <div className='cabecalho'>
+          <img src='https://github.com/V-Matheus.png' alt=''/>
+      </div>
+      <div className='rodape'>
+          <h4>Victor Matheus</h4>
+          <h5>Dev</h5>
+      </div>
+  </div>)
+}
+
--- organo/src/components/Time/index.jsx
+++ organo/src/components/Time/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './Time.css'
+import { Colaborador } from '../Colaborador'
 
 export const Time = ({nome, corPrimaria, corSecundaria}) => {
   return (
     <section style={{backgroundColor: corSecundaria, }} className='time'>
         <h3 style={{borderColor: corPrimaria}}>{nome}</h3>
+        <Colaborador />
     </section>
   )
 }
