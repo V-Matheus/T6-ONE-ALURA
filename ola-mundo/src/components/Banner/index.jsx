5bd75452113640d6933a59c606a195bfa60d2346
Victor Matheus <victormatheus507@gmail.com>
2024-06-12 15:05:46 -0300

    Desenvolvendo SobreMim

--- ola-mundo/src/components/Banner/index.jsx
+++ ola-mundo/src/components/Banner/index.jsx
@@ -9,7 +9,7 @@ export const Banner = () => {
       <div className={styles.apresentacao}>
         <h1 className={styles.titulo}>Olá Mundo</h1>
 
-        <p className={styles.paragrafo}></p>
+        <p className={styles.paragrafo}>Boas vindas ao meu espaço pessoal! Eu sou Antônio Evaldo, instrutor de Front-end da Alura. Aqui compartilho vários conhecimentos, espero que aprenda algo novo :)</p>
       </div>
 
       <div className={styles.imagens}>
