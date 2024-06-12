c08ee78231d118860522afac43b56ef8925549e7
Victor Matheus <victormatheus507@gmail.com>
2024-06-12 15:20:59 -0300

    Dinamizando os dados do Post

--- ola-mundo/src/paginas/Post/index.jsx
+++ ola-mundo/src/paginas/Post/index.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import styles from './Post.module.css'
-import { useParams } from 'react-router-dom'
+import React from 'react';
+import styles from './Post.module.css';
+import { useParams } from 'react-router-dom';
+import posts from '../../assets/json/posts.json';
+import { PostModelo } from '../../components/PostModelo';
 
 export const Post = () => {
+  const parametros = useParams();
+  const post = posts.find((post) => {
+    return post.id === Number(parametros.id);
+  });
 
-  const parametros = useParams()
+  const imagePath = require(`../../assets/posts/${post.id}/capa.png`);
 
   return (
-    <div>Post {parametros.id}</div>
+    <PostModelo fotoCapa={imagePath} titulo={post.titulo}>
+      Texto
+    </PostModelo>
   )
-}
+};
