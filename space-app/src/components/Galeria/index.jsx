19e5488537e250c2bdca1738fb3e2ac9fbb33e86
Victor Matheus <victormatheus507@gmail.com>
2024-06-13 13:50:05 -0300

    Estruturando a galeria

--- space-app/src/components/Galeria/index.jsx
+++ space-app/src/components/Galeria/index.jsx
@@ -1,6 +1,26 @@
+import styled from 'styled-components';
+import { Titulo } from '../Titulo';
+import { Populares } from './Populares';
+import { Tags } from './Tags';
+
+const GaleriaContainer = styled.div`
+  display: flex;
+`
+
+const SessaoFluida = styled.section`
+  flex-grow: 1;
+`
 
 export const Galeria = () => {
   return (
-    <h2>Galeria</h2>
-  )
-}
+    <>
+      <Tags />
+      <GaleriaContainer>
+        <SessaoFluida>
+          <Titulo>Galeria</Titulo>
+        </SessaoFluida>
+        <Populares />
+      </GaleriaContainer>
+    </>
+  );
+};
